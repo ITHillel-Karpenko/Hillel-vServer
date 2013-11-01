8314e90dc0d339d2e43c2be2471f6300971525d2
Vladislav Karpenko <vladislav@karpenko.od.ua>
2013-11-01 11:07:33 +0200

    Add main and Server thread

--- src/VServer.java
+++ src/VServer.java
@@ -4,16 +4,40 @@
  * Date: 31.10.13
  * Time: 12:22
  */
-public class VServer {
-    private String receivedData;
+public class VServer implements Runnable{
+    private VStream[] streams;
+    private Storage storage;
 
-    public void read(VStream stream)
-    {
-        receivedData = stream.read();
-        stream.confirm();
+    public static void main(String[] args) {
+        VServer vServer = new VServer(VStream[], Storage);
+        Thread thread = new Thread(vServer);
+        thread.start();
+
     }
 
-    public void save(Storage storage) {
+    public VServer(VStream[] vStreams, Storage storage) {
+        this.streams = vStreams;
+        this.storage = storage;
+    }
+
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            for (int i=0; i < streams.length; i++) {
+                String receivedData = this.read(streams[i]);
+                if (receivedData==null) continue;
+                this.save(storage, receivedData);
+            }
+        }
+    }
+
+    private synchronized String read(VStream stream)
+    {
+        String receivedData = stream.read();
+        if(receivedData!=null) stream.confirm();
+        return receivedData;
+    }
+
+    private synchronized void save(Storage storage, String receivedData) {
         Buffer buffer = new Buffer(storage);
         buffer.writeString(encode(receivedData));
 
